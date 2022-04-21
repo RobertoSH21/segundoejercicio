0d5636f811e144401d0d5068cd7f6087443c0fee
RobertoSH <al217053@alumnos.uacj.mx>
2022-04-21 16:15:00 -0600

    viendo el error de clase no definida

--- Spiderverse/test/Spiderman.test.js
+++ Spiderverse/test/Spiderman.test.js
@@ -1,8 +1,13 @@
 
 
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+describe("Unit Tests for Spiderman class", () => {
+    test('1) create an spiderman object', () => {
+      const AndrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+      
+      expect(AndrewGarfield.name).toBe("Spiderman Sony")
+      expect(AndrewGarfield.age).toBe(31)
+      expect(AndrewGarfield.actor).toBe("Andrew Garfield")
+      expect(AndrewGarfield.movies).toBe(2)
+      expect(AndrewGarfield.studio).toBe("Sony")
     });
   })
